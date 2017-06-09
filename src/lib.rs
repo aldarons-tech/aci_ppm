191d557734dcfce10066e511fc552ed14b0dbf14
Jeron Aldaron Lau <jeron.lau@plopgrizzly.tech>
2017-06-08 20:11:19 -0500

    Added code

--- src/lib.rs
+++ src/lib.rs
@@ -4,51 +4,75 @@
 // Copyright 2017 (c) Jeron Lau
 // Licensed under the MIT LICENSE
 
+#![no_std]
+
+pub enum ErrPPM {
+	NotPPM,
+	BadNum,
+}
+
+impl ::core::fmt::Debug for ErrPPM {
+	fn fmt(&self, f: &mut ::core::fmt::Formatter) -> ::core::fmt::Result {
+		write!(f, "Couldn't parse PPM because: {}", match *self {
+			ErrPPM::NotPPM => "Not a PPM file (bad header)",
+			ErrPPM::BadNum => "Dimensions are not numbers",
+		})
+	}
+}
+
 fn skip_line(ppm: &'static [u8], index: &mut usize) {
-	loop {
-		if ppm[*index] == '\n' as u8 {
-			break;
-		}
+	while ppm[*index] != b'\n' {
 		*index += 1;
 	}
 	*index += 1;
 }
 
-fn utf8_to_u32(ppm: &'static [u8], index: &mut usize, until: char) -> u32 {
+fn utf8_to_u32(ppm: &'static [u8], index: &mut usize, until: u8)
+	-> Result<u32, ErrPPM>
+{
+	let zero = b'0';
 	let mut number = 0;
-	while ppm[*index] != until as u8 {
+
+	while ppm[*index] != until {
+		let digit = ppm[*index];
+
 		number *= 10;
-		if ppm[*index] != '0' as u8 {
-			number += ppm[*index] as u32 - 48;
+		if digit != zero {
+			if digit < zero || digit > zero + 9 {
+				return Err(ErrPPM::BadNum);
+			}
+			number += (digit - zero) as u32;
 		}
 		*index += 1;
 	}
 	*index += 1;
-	number
+	Ok(number)
 }
 
 /// Decode PPM data.
 /// 
 /// Returns `(width, height, pixels)`
-pub fn decode(ppm: &'static [u8]) -> (u32, u32, &'static [u8]) {
+pub fn decode(ppm: &'static [u8])
+	-> Result<(u32, u32, &'static [u8]), ErrPPM>
+{
 	let mut index = 3;
 
 	// Header
-	if ppm[0] != 'P' as u8 || ppm[1] != '6' as u8 {
-		panic!("Not a PPM file.");
+	if ppm[0] != b'P' || ppm[1] != b'6' {
+		return Err(ErrPPM::NotPPM);
 	}
 
 	// Optional Comment
-	if ppm[index] == '#' as u8 {
+	if ppm[index] == b'#' {
 		skip_line(ppm, &mut index);
 	}
 
 	// Width & Height
-	let width = utf8_to_u32(ppm, &mut index, ' ');
-	let height = utf8_to_u32(ppm, &mut index, '\n');
+	let width = utf8_to_u32(ppm, &mut index, b' ')?;
+	let height = utf8_to_u32(ppm, &mut index, b'\n')?;
 
 	// We don't care about this.  In ppm format 255 is normally here
 	skip_line(ppm, &mut index);
 
-	(width, height, &ppm[index..])
+	Ok((width, height, &ppm[index..]))
 }
