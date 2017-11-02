3d1f14b6ea1ccde33745e8a04dc8d48b7fbf7728
Jeron Aldaron Lau <jeron.lau@plopgrizzly.com>
2017-11-02 14:13:41 -0500

    aci_ppm new version.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,20 @@
-// lib.rs
-// Aldaron's Codec Interface - PPM
-// Copyright 2017 (c) Aldaron's Tech
-// Copyright 2017 (c) Jeron Lau
+// Aldaron's Codec Interface / PPM
+// Copyright (c) 2017 Plop Grizzly, Jeron Lau <jeron.lau@plopgrizzly.com>
 // Licensed under the MIT LICENSE
+//
+// src/lib.rs
+
+//! Aldaron's Codec Interface / PPM is a small library developed by Plop Grizzly
+//! for decoding ppm image files.
 
 #![no_std]
+#![warn(missing_docs)]
+#![doc(html_logo_url = "http://plopgrizzly.com/icon.png",
+	html_favicon_url = "http://plopgrizzly.com/icon.png",
+	html_root_url = "http://plopgrizzly.com/aci_png/")]
+
+extern crate afi;
+extern crate ami;
 
 /// The errors that can be returned if `decode()` fails.
 pub enum DecodeErrorPPM {
@@ -52,11 +62,8 @@ fn utf8_to_u32(ppm: &'static [u8], index: &mut usize, until: u8)
 	Ok(number)
 }
 
-/// Decode PPM data.  On success, returns image as tuple:
-/// `(width, height, pixels)`
-pub fn decode(ppm: &'static [u8])
-	-> Result<(u32, u32, &'static [u8]), DecodeErrorPPM>
-{
+/// Decode PPM data.  On success, returns image as a `Graphic`.
+pub fn decode(ppm: &'static [u8]) -> Result<afi::Graphic, DecodeErrorPPM> {
 	let mut index = 3;
 
 	// Header
@@ -73,8 +80,32 @@ pub fn decode(ppm: &'static [u8])
 	let width = utf8_to_u32(ppm, &mut index, b' ')?;
 	let height = utf8_to_u32(ppm, &mut index, b'\n')?;
 
+	// Allocate RGBA data.
+	let size = (width * height) as usize;
+	let mut out : ami::Vec<u32> = ami::Vec::with_capacity(size + 2);
+
+	out.push(width);
+	out.push(height);
+
 	// We don't care about this.  In ppm format 255 is normally here
 	skip_line(ppm, &mut index);
 
-	Ok((width, height, &ppm[index..]))
+	// Build Graphic
+	let graphic = {
+		let buf = &ppm[index..];
+		let mut pixel : [u8;4] = [0xFF, 0xFF, 0xFF, 0xFF];
+
+		for i in 0..size {
+			pixel[0] = buf[i * 4 + 0];
+			pixel[1] = buf[i * 4 + 1];
+			pixel[2] = buf[i * 4 + 2];
+			pixel[3] = buf[i * 4 + 3];
+
+			out.push(unsafe {::core::mem::transmute(pixel)});
+		}
+
+		afi::GraphicBuilder::new().rgba(out)
+	};
+
+	Ok(graphic)
 }
